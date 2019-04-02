af51769762fe41ed8049321fa96bcf1cabd99137
Brandon L. Borrero <borrero509@outlook.com>
2019-04-02 14:05:14 -0700

    bug fixes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bcrypt = require("bcryptjs");
 
 const db = require("./database/dbConfig.js");
-const Users = require("./models/users-model.js");
+const Users = require("./models/user-model.js");
 
 const server = express();
 
@@ -41,7 +41,7 @@ server.post("/api/login", (req, res) => {
       if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
-        res.status(401).json({ message: "Invalid Credentials" });
+        res.status(401).json({ message: "You shall not pass!" });
       }
     })
     .catch(error => {
